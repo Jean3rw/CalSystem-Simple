7bdecacb59a80a21e46d4ff6a95729f1b7ce868b
Jean3rw <88708808+Jean3rw@users.noreply.github.com>
2021-08-19 12:42:00 +0200

    Create Program.cs
    Commit To main

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SimpleMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This system calculates the sum of two numbers");
+            Console.WriteLine("Enter the first Number: ");
+            int num1 = Console.Read();
+
+            Console.WriteLine("Enter Second Number: ");
+            int num2 = Console.Read();
+
+            if (num1 + num2 > 0)
+            {
+                int Call = num1 + num2;
+                Console.WriteLine("The Sum is= " + Call);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+
+        }
+    }
